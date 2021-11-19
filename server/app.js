e3943705e574c89356c28ae21de47d2187b96a6b
Travis Pak <tpak415@gmail.com>
2021-11-18 21:22:50 -0500

    Create routes to get all questions/answers, add question/answer

--- server/app.js
+++ server/app.js
@@ -3,12 +3,68 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const db = require('./db/index.js');
 
 app.use(express.json());
+// app.use(express.responseTime());
 // app.use(express.static())
 
-app.get('/', function (req, res) {
-  res.send('hello world')
+
+// GET ALL QUESTIONS FOR GIVEN PRODUCT
+app.get('/qa/questions', (req, res) => {
+  let query = req.query;
+
+  db.getQuestions(query.product_id, query.page, query.count)
+    .then(questions => {
+      res.status(200).send(JSON.stringify(questions))})
+    .catch(err => {console.log(err); res.status(500).send(err)});
 })
+
+// GET ALL ANSWERS FOR A GIVEN QUESTION
+app.get('/qa/questions/:question_id/answers', (req, res) => {
+  let params = req.params;
+  let query = req.query;
+
+  db.getAnswers(params.question_id, query.page, query.count)
+    .then(answers => {
+      console.log(answers);
+      res.status(200).send(JSON.stringify(answers))})
+    .catch(err => { res.status(500).send(err) });
+})
+
+// ADD A NEW QUESTION FOR A PRODUCT
+app.post('/qa/questions', (req, res) => {
+  console.log('POST QUESTION: \n', req.body);
+  let body = req.body;
+  let newEntryTime = Math.round(new Date().getTime()).toString();
+
+  db.addQuestion(body.body, body.name, body.email, body.product_id, newEntryTime)
+  .then(answers => {
+    console.log(answers);
+    res.status(201).send('created')
+  })
+  .catch(err => { res.status(500).send(err) });
+})
+
+// ADD A NEW ANSWER FOR A QUESTION
+app.post('/qa/questions/:question_id/answers', (req, res) => {
+  console.log('POST ANSWER: \n', req.params);
+  let body = req.body;
+  let params = req.params;
+  let newEntryTime = Math.round(new Date().getTime()).toString();
+
+  db.addAnswer(params.question_id, body.body, newEntryTime, body.name, body.email, body.photos)
+  .then(response => {
+    console.log(response);
+    res.status(201).send('created')
+  })
+  .catch(err => { res.status(500).send(err) });
+})
+
+// MARK A QUESTION AS HELPFUL
+app.put('/qa/questions/:question_id/helpful', (req, res) => {
+  
+})
+
 
 app.listen(3000, () => {console.log('listening on port 3000')})
--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,3 @@
-// const postgres = require('postgres')
-
-// const sql = postgres({ ...options }) // will default to the same as psql
-
-// await sql`
-//   select name, age from users
-// `
-
 
 const {Pool, Client} = require('pg');
 
@@ -13,30 +5,54 @@ const pool = new Pool({
   database: 'qa'
 });
 
-// pool.query('select * from questions where id < 100', (err, res) => {
-//   console.log('error: ', err, '\nresponse: ', res.rows);
-//   pool.end();
-// })
-
-const getQuestions = (productId) => {
-  pool.query(`select * from questions where product_id = ${productId}`, (err, res) => {
-    console.log(res.rows);
-    return res.rows;
-  })
+// GET ALL QUESTIONS FOR GIVEN PRODUCT
+module.exports.getQuestions = (productId, page = 1, count = 5) => {
+  // console.log(productId, page, count)
+  return pool.query(`
+  SELECT *
+  FROM questions
+  WHERE product_id = ${productId}
+  ORDER BY id
+  OFFSET ${page * count - count}
+  FETCH NEXT ${count} ROWS ONLY;`)
+    .then(res => res.rows)
+    .catch(err => err);
 }
 
-const getAnswers = (questionId) => {
-  pool.query(`select * from answers where question_id = ${questionId}`, (err, res) => {
-    console.log(res.rows);
-    return res.rows;
-  })
+// GET ALL ANSWERS FOR A GIVEN QUESTION
+module.exports.getAnswers = (questionId, page = 1, count = 5) => {
+  return pool.query(`
+  SELECT answers.id, answers.body, answers.date_written,
+    answers.answerer_name, answers.answerer_email, answers.reported, answers.helpful,
+    COALESCE (json_agg(json_build_object('id', photos.id, 'answer id', answer_id, 'url', photos.url)) FILTER (WHERE photos.id IS NOT NULL), '[]')
+     AS photos
+  FROM answers
+  LEFT JOIN photos ON answers.id = photos.answer_id
+  WHERE answers.question_id = ${questionId}
+  GROUP BY answers.id;
+  `)
+  .then(res => res.rows)
+  .catch(err => err)
 }
 
-const getPhotos = (answerId) => {
-  pool.query(`select * from photos where answer_id = ${answerId}`, (err, res) => {
-    console.log(res.rows);
-    return res.rows;
-  })
+// ADD A NEW QUESTION FOR A PRODUCT
+module.exports.addQuestion = (body, name, email, productId, time) => {
+  console.log('addQuestion to db: ', body, name, email, productId, time)
+  return pool.query(`
+    INSERT INTO questions (id, product_id, body, date_written, asker_name, asker_email, reported, helpful)
+    VALUES (default, ${productId}, '${body}', ${time}, '${name}', '${email}', false, 0);
+  `)
+  .then(res => res.rows)
+  .catch(err => err)
 }
 
-getPhotos(68543);
+// ADD A NEW ANSWER FOR A QUESTION
+module.exports.addAnswer = (questionId, body, time, name, email, photos) => {
+  // console.log('addAnswer to db: ', questionId, body, name, email, photos, time)
+  return pool.query(`
+    INSERT INTO answers (id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful)
+    VALUES (default, ${questionId}, '${body}', ${time}, '${name}', '${email}', false, 0);
+  `)
+  .then(res => res.rows)
+  .catch(err => err)
+}
