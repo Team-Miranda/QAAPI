a0d5f26d70dba95618a43c5f10f6cd04338c6d53
Travis Pak <tpak415@gmail.com>
2021-11-23 11:18:16 -0500

    Fix formatting for all response data, install artillery and begin performance testing

--- server/app.js
+++ server/app.js
@@ -2,98 +2,116 @@
 
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const path = require('path');
 const db = require('./db/index.js');
 
 app.use(express.json());
+app.use(morgan('--:method - :url --- response-time: :response-time'))
 // app.use(express.responseTime());
 // app.use(express.static())
-
 
 // GET ALL QUESTIONS FOR GIVEN PRODUCT
 app.get('/qa/questions', (req, res) => {
   let query = req.query;
 
   db.getQuestions(query.product_id, query.page, query.count)
-    .then(questions => {
-      res.status(200).send(JSON.stringify(questions))})
-    .catch(err => {console.log(err); res.status(500).send(err)});
-})
+    .then((questions) => {
+      res.status(200).send({
+        product_id: query.product_id,
+        results: questions,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send(err.stack);
+    });
+});
 
 // GET ALL ANSWERS FOR A GIVEN QUESTION
 app.get('/qa/questions/:question_id/answers', (req, res) => {
   let params = req.params;
   let query = req.query;
+  query.page;
 
   db.getAnswers(params.question_id, query.page, query.count)
-    .then(answers => {
-      console.log(answers);
-      res.status(200).send(JSON.stringify(answers))})
-    .catch(err => { res.status(500).send(err) });
-})
+    .then((answers) => {
+      res.status(200).send({
+        qestion: params.question_id,
+        page: query.page || 1,
+        count: query.count || 5,
+        results: answers,
+      });
+    })
+    .catch((err) => {
+      res.status(500).send(err.stack);
+    });
+});
 
 // ADD A NEW QUESTION FOR A PRODUCT
 app.post('/qa/questions', (req, res) => {
-  console.log('POST QUESTION: \n', req.body);
+  console.log('\nPOST QUESTION: \n', req.body.body);
   let body = req.body;
   let newEntryTime = Math.round(new Date().getTime()).toString();
 
   db.addQuestion(body.body, body.name, body.email, body.product_id, newEntryTime)
-  .then(answers => {
-    console.log(answers);
-    res.status(201).send('created')
-  })
-  .catch(err => { res.status(500).send(err) });
-})
+    .then((answers) => {
+      console.log(answers);
+      res.status(201).send('CREATED');
+    })
+    .catch((err) => {
+      res.status(500).send(err.stack);
+    });
+});
 
 // ADD A NEW ANSWER FOR A QUESTION
 app.post('/qa/questions/:question_id/answers', (req, res) => {
-  console.log('POST ANSWER \n');
+  console.log('\nPOST ANSWER: \n', req.body.body);
   let body = req.body;
   let params = req.params;
   let newEntryTime = Math.round(new Date().getTime()).toString();
 
   db.addAnswer(params.question_id, body.body, newEntryTime, body.name, body.email, body.photos)
-  .then(response => {
-    // console.log('response from db: ', response);
-    res.status(201).send('created')
-  })
-  .catch(err => { res.status(500).send(err) });
-})
+    .then((response) => {
+      res.status(201).send('CREATED');
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
 
 // MARK A QUESTION AS HELPFUL
 app.put('/qa/questions/:question_id/helpful', (req, res) => {
   console.log('UPDATE AS HELPFUL FOR question_id: ', req.params.question_id);
 
   db.updateQuestionHelpful(req.params.question_id)
-    .then(success => res.status(204).send('marked as helpful'))
-    .catch(err => res.status(500).send(err))
-
-})
+    .then((success) => res.status(204).send('marked as helpful'))
+    .catch((err) => res.status(500).send(err));
+});
 
 // REPORT A QUESTION
 app.put('/qa/questions/:question_id/report', (req, res) => {
   db.reportQuestion(req.params.question_id)
-    .then(success => res.status(204).send('reported'))
-    .catch(err => res.status(500).send(err))
-})
+    .then((success) => res.status(204).send('reported'))
+    .catch((err) => res.status(500).send(err));
+});
 
 // MARK AN ANSWER AS HELPFUL
 app.put('/qa/answers/:answer_id/helpful', (req, res) => {
   console.log('UPDATE AS HELPFUL FOR answer_id: ', req.params.answer_id);
 
   db.updateAnswerHelpful(req.params.answer_id)
-    .then(success => res.status(204).send('marked as helpful'))
-    .catch(err => res.status(500).send(err))
-
-})
+    .then((success) => res.status(204).send('marked as helpful'))
+    .catch((err) => res.status(500).send(err));
+});
 
 // REPORT AN ANSWER
 app.put('/qa/answers/:answer_id/report', (req, res) => {
-
   db.reportAnswer(req.params.answer_id)
-    .then(success => res.status(204).send('reported'))
-    .catch(err => res.status(500).send(err))
-})
+    .then((success) => res.status(204).send('reported'))
+    .catch((err) => res.status(500).send(err));
+});
 
-app.listen(3000, () => {console.log('listening on port 3000')})
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
--- server/db/index.js
+++ server/db/index.js
@@ -1,56 +1,74 @@
-
-const {Pool, Client} = require('pg');
+const { Pool, Client } = require("pg");
 
 const pool = new Pool({
-  database: 'qa'
+  database: "qa",
 });
 
 // GET ALL QUESTIONS FOR GIVEN PRODUCT
 module.exports.getQuestions = (productId, page = 1, count = 5) => {
-  // console.log(productId, page, count)
-  return pool.query(`
-  SELECT *
-  FROM questions
-  WHERE product_id = ${productId}
-  ORDER BY id
-  OFFSET ${page * count - count}
-  FETCH NEXT ${count} ROWS ONLY;`)
-    .then(res => res.rows)
-    .catch(err => err);
-}
+  return pool
+    .query(
+      `
+    SELECT id, product_id, body, to_timestamp(date_written/1000), asker_name, asker_email, reported, helpful AS helpfulness
+    FROM questions
+    WHERE product_id = ${productId} AND reported = false
+    ORDER BY id
+    OFFSET ${page * count - count}
+    FETCH NEXT ${count} ROWS ONLY;
+  `
+    )
+    .then((res) => res.rows)
+    .catch((err) => {
+      throw err;
+    });
+};
 
 // GET ALL ANSWERS FOR A GIVEN QUESTION
 module.exports.getAnswers = (questionId, page = 1, count = 5) => {
-  return pool.query(`
-  SELECT answers.id, answers.body, answers.date_written,
+  return pool
+    .query(
+      `
+    SELECT answers.id AS answer_id, answers.body, to_timestamp(answers.date_written/1000) AS date,
     answers.answerer_name, answers.answerer_email, answers.reported, answers.helpful,
     COALESCE (json_agg(json_build_object('id', photos.id, 'answer id', answer_id, 'url', photos.url)) FILTER (WHERE photos.id IS NOT NULL), '[]')
      AS photos
-  FROM answers
-  LEFT JOIN photos ON answers.id = photos.answer_id
-  WHERE answers.question_id = ${questionId}
-  GROUP BY answers.id;
-  `)
-  .then(res => res.rows)
-  .catch(err => err)
-}
+    FROM answers
+    LEFT JOIN photos ON answers.id = photos.answer_id
+    WHERE answers.question_id = ${questionId}  AND answers.reported = false
+    GROUP BY answers.id;
+  `
+    )
+    .then((res) => res.rows)
+    .catch((err) => {
+      throw err;
+    });
+};
 
 // ADD A NEW QUESTION FOR A PRODUCT
 module.exports.addQuestion = (body, name, email, productId, time) => {
-  console.log('addQuestion to db: ', body, name, email, productId, time)
-  return pool.query(`
+  return pool
+    .query(
+      `
     INSERT INTO questions (id, product_id, body, date_written, asker_name, asker_email, reported, helpful)
     VALUES (default, ${productId}, '${body}', ${time}, '${name}', '${email}', false, 0);
-  `)
-  .then(res => res.rows)
-  .catch(err => err)
-}
+  `
+    )
+    .then((res) => res.rows)
+    .catch((err) => {throw err});
+};
 
 // ADD A NEW ANSWER FOR A QUESTION
 module.exports.addAnswer = (questionId, body, time, name, email, photoURLs) => {
-  photoURLs = '[' + photoURLs.map((each) => {return `'${each}'`}) + ']';
+  photoURLs =
+    "[" +
+    photoURLs.map((each) => {
+      return `'${each}'`;
+    }) +
+    "]";
 
-  return pool.query(`
+  return pool
+    .query(
+      `
     WITH answer_insert AS (
     INSERT INTO answers (id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful)
     VALUES (default, ${questionId}, '${body}', ${time}, '${name}', '${email}', false, 0)
@@ -58,51 +76,66 @@ module.exports.addAnswer = (questionId, body, time, name, email, photoURLs) => {
 
     INSERT INTO photos (id, answer_id, url)
     VALUES (default, (SELECT id FROM answer_insert), unnest(array${photoURLs}));
-  `)
-    .then(res => res)
-    .catch(err => err)
-}
+  `
+    )
+    .then((res) => res)
+    .catch((err) => {throw err});
+};
 
 // INCREMENT A QUESTION'S HELPFUL COUNT
 module.exports.updateQuestionHelpful = (questionId) => {
-  return pool.query(`
+  return pool
+    .query(
+      `
     UPDATE questions
     SET helpful = (helpful + 1)
     WHERE id = ${questionId}
-  `)
-    .then(res => res)
-    .catch(err => err)
-}
+  `
+    )
+    .then((res) => res)
+    .catch((err) => {throw err});
+};
 
 // REPORT A QUESTION
 module.exports.reportQuestion = (questionId) => {
-  return pool.query(`
+  return pool
+    .query(
+      `
     UPDATE questions
     SET reported = true
     WHERE id = ${questionId}
-  `)
-    .then(res => res)
-    .catch(err => err)
-}
+  `
+    )
+    .then((res) => res)
+    .catch((err) => {throw err});
+};
 
 // INCREMENT AN ANSWERS'S HELPFUL COUNT
 module.exports.updateAnswerHelpful = (answerId) => {
-  return pool.query(`
+  return pool
+    .query(
+      `
     UPDATE answers
     SET helpful = (helpful + 1)
     WHERE id = ${answerId}
-  `)
-    .then(res => res)
-    .catch(err => err)
-}
+  `
+    )
+    .then((res) => res)
+    .catch((err) => {throw err});
+};
 
 // REPORT AN ANSWER
 module.exports.reportAnswer = (answerId) => {
-  return pool.query(`
+  return pool
+    .query(
+      `
     UPDATE answers
     SET reported = true
     WHERE id = ${answerId}
-  `)
-    .then(res => res)
-    .catch(err => err)
-}
+  `
+    )
+    .then((res) => res)
+    .catch((err) => {throw err});
+};
+
+// SELECT to_timestamp(question_date/1000):: timestamp, question_id from questions limit 10;
